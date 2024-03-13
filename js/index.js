5f7ce0eea038dd9d1f8e1f65d4ff227fe22f0ec4
yeco0004 <63363566+yeco0004@users.noreply.github.com>
2024-03-13 10:58:41 -0500

    index.js

--- js/index.js
+++ js/index.js
@@ -3,3 +3,36 @@ let numeros = [];
 for (let i = 0; i < 200; i++) {
   numeros[i] = i+1;
 }
+//numeros impares
+function impares(numeros){
+  let impares = numeros.filter(x => x % 2 !== 0).length
+  return impares
+}
+
+
+//numero pares
+function pares(numeros){
+  let pares = numeros.filter(x => x % 2 === 0).length
+  return pares
+}
+
+//datos del selector
+const seleccion = document.getElementById('opcionesSelect')
+alert(seleccion)
+function selector(){
+
+}
+
+
+
+//mostrar datos
+document.getElementById('VerBtn').addEventListener('click' , ()=>{
+
+if (seleccion == 0) {
+  
+} else {
+  
+  
+}
+
+})
