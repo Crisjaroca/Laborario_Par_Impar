9ff37fc67fb05dd4d4baefe98751451c29ac8e8a
Crisjaroca <crijavrodriguez@uniboyaca.edu.co>
2024-03-17 14:42:46 -0500

    Arreglos finales
    Se agregó información visual para los pares e impares mayores y se hicieron unos arreglos en el código.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 // Se espera a que el DOM esté completamente cargado para ejecutar el código
 document.addEventListener("DOMContentLoaded", function() {
+  
   // Se crea un array de números del 1 al 200
   let numeros = [];
   for (let i = 1; i <= 200; i++) {
@@ -15,7 +16,13 @@ document.addEventListener("DOMContentLoaded", function() {
   // Se agrega un evento para detectar cambios en el selector de opciones
   opcionesSelect.addEventListener('change', function() {
     var seleccion = opcionesSelect.value;
-    inputHabilitado.disabled = seleccion === 'paresmax' || seleccion === 'imparesmax' ? false : true;
+    if (seleccion === 'paresmax' || seleccion === 'imparesmax') {
+      inputHabilitado.style.display = 'block';
+      textoMayores.style.display = 'block';
+    } else {
+      inputHabilitado.style.display = 'none';
+      textoMayores.style.display = 'none';
+    }
     inputHabilitado.value = '';
   });
 
@@ -26,26 +33,20 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Se ejecuta la operación según la opción seleccionada
     if (seleccion === 'pares') {
-      // Se filtran los números pares, se invierte el orden y se crea un array con los resultados formateados
       let pares = numeros.filter(num => num % 2 === 0).reverse().map((num, index) => 'Par en la posición ' + (num - 1) + ': ' + num);
       resultadoLista.innerHTML = pares.join('<br>');
     } else if (seleccion === 'impares') {
-      // Se filtran los números impares, se invierte el orden y se crea un array con los resultados formateados
       let impares = numeros.filter(num => num % 2 !== 0).reverse().map((num, index) => 'Impar en la posición ' + (num - 1) + ': ' + num);
       resultadoLista.innerHTML = impares.join('<br>');
     } else if (seleccion === 'paresmax') {
-      // Se verifica que el valor del input sea un número válido y se encuentra entre 0 y 200
       if (!isNaN(inputValue) && inputValue >= 0 && inputValue <= 200) {
-        // Se filtran los números pares mayores al valor del input, se invierte el orden y se crea un array con los resultados formateados
         let paresMax = numeros.filter(num => num % 2 === 0 && num > inputValue).reverse().map((num, index) => 'Par mayor a ' + inputValue + ' en la posición ' + (num - 1) + ': ' + num);
         resultadoLista.innerHTML = paresMax.join('<br>');
       } else {
         alert('Por favor, ingresa un número válido (entre 0 y 200) para la opción "Pares Mayores".');
       }
     } else if (seleccion === 'imparesmax') {
-      // Se verifica que el valor del input sea un número válido y se encuentra entre 0 y 200
       if (!isNaN(inputValue) && inputValue >= 0 && inputValue <= 200) {
-        // Se filtran los números impares mayores al valor del input, se invierte el orden y se crea un array con los resultados formateados
         let imparesMax = numeros.filter(num => num % 2 !== 0 && num > inputValue).reverse().map((num, index) => 'Impar mayor a ' + inputValue + ' en la posición ' + (num - 1) + ': ' + num);
         resultadoLista.innerHTML = imparesMax.join('<br>');
       } else {
